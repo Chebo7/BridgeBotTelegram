6c80f392a33a327e4e368b55354444d7d598a59f
Chebo7 <192.cpp@gmail.com>
2025-12-21 22:52:20 +0300

    Changed output via cout, and also made the logic for changing unordered_map fields more secure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
-#include "tgbot/types/BotCommandScope.h"
-#include "tgbot/types/BotCommandScopeChat.h"
-#include "tgbot/types/Chat.h"
-#include "tgbot/types/Message.h"
-#include <cstdint>
-#include <cstdlib>
-#include <fstream>
 #include <iostream>
-#include <stdio.h>
 #include <string>
 #include <tgbot/tgbot.h>
 #include <unordered_map>
@@ -15,6 +7,8 @@ enum ClientState { Default, WaitingForText };
 
 int main() {
   const char *TOKEN = getenv("TELEGRAM_BOT_TOKEN");
+  const int64_t ADMIN_CHAT_ID{};
+
   std::unordered_map<int64_t, ClientState> Clients;
 
   TgBot::Bot bot(TOKEN);
@@ -25,42 +19,39 @@ int main() {
         "Hey, if you want to send me a private message, leave a short message "
         "here explaining what happened or why you're writing, and I'll see "
         "your request.Thanks, this is to prevent spam and bots :)     ");
-    Clients.emplace(message->chat->id, ClientState::WaitingForText);
+    Clients.insert_or_assign(message->chat->id, ClientState::WaitingForText);
   });
 
   bot.getEvents().onNonCommandMessage(
-      [&bot, &Clients](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
-          return;
-        }
+      [&bot, &Clients, ADMIN_CHAT_ID](TgBot::Message::Ptr message) {
+        std::cout << "User wrote " + message->text << std::endl;
 
-        if (Clients[message->chat->id] == 0) {
+        if (!Clients.contains(message->chat->id)) {
           bot.getApi().sendMessage(
               message->chat->id,
               "Hmmm... I haven't learned to understand this command :(");
         } else if (Clients[message->chat->id] == ClientState::WaitingForText) {
           bot.getApi().sendMessage(message->chat->id,
                                    "Your message has been sent.");
-          Clients[message->chat->id] = ClientState::Default;
+          Clients.insert_or_assign(message->chat->id, ClientState::Default);
 
           std::string clientUsername = message.get()->chat->username;
 
-          bot.getApi().sendMessage(7960664163,
+          bot.getApi().sendMessage(ADMIN_CHAT_ID,
                                    "The @" + clientUsername +
                                        " wrote to you: " + message->text);
         }
       });
 
   try {
-    printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+    std::cout << "Bot username: " + bot.getApi().getMe()->username << std::endl;
     TgBot::TgLongPoll longPoll(bot);
     while (true) {
-      printf("Long poll started\n");
+      std::cout << "Long poll started" << std::endl;
       longPoll.start();
     }
   } catch (TgBot::TgException &e) {
-    printf("error: %s\n", e.what());
+    std::cout << "Error: " << e.what();
   }
   return 0;
 }
